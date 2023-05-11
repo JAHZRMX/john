72c84c7422505b5409a961ee2d2fe30ec7de6254
JAHZRMX <112454702+JAHZRMX@users.noreply.github.com>
2023-05-11 17:20:59 -0600

    Add files via upload

---
+++ proyecto 122/main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+
+  square(290,290,90,90);
+  triangle(90,90,90,90,90,90);
+  circle(350, 50, 70);
+  
+}
